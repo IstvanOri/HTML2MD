6325c2c9ed522d5b38d6ef0405f98869c46483ee
IstvanOri <ori@east1.hu>
2020-03-25 22:19:45 +0100

    Fixing local links. .html -> .md

---
+++ transformers/LinkFixer.py
@@ -0,0 +1,21 @@
+import re
+from transformers.Tranformation import Transformation
+
+
+def _sanitize(input_):
+    if input_ == "\\n":
+        return "\n"
+    if input_ == "\\\\n":
+        return "\\n"
+    if input_ == "\\s":
+        return "\\s"
+    return input_
+
+
+class LinkFixer(Transformation):
+
+    def __init__(self, args):
+        pass
+
+    def execute(self, content: str) -> str:
+        return re.sub('\\((?!.*http://)(.*)(.html)\\)', r'(\g<1>.md)', content)
