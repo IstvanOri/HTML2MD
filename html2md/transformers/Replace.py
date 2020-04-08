25c6cc9eeddfbf2b100f79754d3ee82c67b66e53
IstvanOri <ori@east1.hu>
2020-04-08 11:29:38 +0200

    Replace transformer now works with patterns

--- html2md/transformers/Replace.py
+++ html2md/transformers/Replace.py
@@ -1,21 +1,13 @@
+import re
+
 from html2md.transformers.Tranformation import Transformation
-
-
-def _sanitize(input_):
-    if input_ == "\\n":
-        return "\n"
-    if input_ == "\\\\n":
-        return "\\n"
-    if input_ == "\\s":
-        return "\\s"
-    return input_
 
 
 class Replace(Transformation):
 
     def __init__(self, args):
-        self._from_ = _sanitize(args[0])
-        self._to_ = _sanitize(args[1])
+        self._from_ = args[0]
+        self._to_ = args[1]
 
     def execute(self, content: str) -> str:
-        return content.replace(self._from_, self._to_)
+        return re.sub(re.compile(self._from_), self._to_, content)
